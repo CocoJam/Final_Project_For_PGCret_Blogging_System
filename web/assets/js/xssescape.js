0205d88e3b6a623a63a501337702a575a4eedbfd
ljam763 <ljam763@aucklanduni.ac.nz>
2017-06-10 18:22:37 +1200

    fixed login

--- web/assets/js/xssescape.js
+++ web/assets/js/xssescape.js
@@ -3,20 +3,19 @@
  */
 $(document).ready(function () {
     var values = /<script[^]*<\/script>/i;
-
+    var php= /<( )*?\?( )*?php[^]*>/i;
     $("textarea").keyup(function () {
-        console.log("Mathcing textarea");
         var content = $(this).val();
-        if (content.match(values)) {
+        if(content.match(values)||content.match(php)){
             $(this).val(content.replace(values, ""));
+            $(this).val(content.replace(php, ""));
         }
     });
     $("input").keyup(function () {
-        console.log("Mathcing input");
         var content = $(this).val();
-        if (content.match(values)) {
+        if(content.match(values)||content.match(php)){
             $(this).val(content.replace(values, ""));
+            $(this).val(content.replace(php, ""));
         }
     });
-    console.log($("textarea"));
 });
