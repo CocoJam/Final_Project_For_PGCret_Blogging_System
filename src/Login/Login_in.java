5383356aa654c71803497b32d80e2b84e9cc8a8e
ljam763 <ljam763@aucklanduni.ac.nz>
2017-06-15 15:05:12 +1200

    "merged"

--- src/Login/Login_in.java
+++ src/Login/Login_in.java
@@ -74,10 +74,11 @@ public class Login_in extends HttpServlet {
             LoginPassing loginPassing = new LoginPassing(); //See loginPassing class: stores all the methods for login including DAO query the database.
 
             if (session.getAttribute("log") != null) {
+                System.out.println("loggin check");
                 if ((boolean) session.getAttribute("log")) {
+                    System.out.println("loggin check");
                     //checking for when a user is logined in and other user tries to login within the same session.
                     //This will logout the first person's account then login the second person. Hence when the second person logout everyone should be logged out.
-
                     //this is to bounce people out if people are already logged in. IF username and password is not in session bounces to profile and logs out the previous user.
                     if (!innerclass.username.equals(session.getAttribute("username")) && !innerclass.password.equals(session.getAttribute("password"))) {
                         if (loginLogic(req, resp, session, loginPassing, innerclass)) {
@@ -85,7 +86,6 @@ public class Login_in extends HttpServlet {
                             req.getRequestDispatcher("/ProfilePage").forward(req, resp);
                             return;
                         }
-
                         //There is a major bug, even though there is someone logged in ppl can still bypass it because it does not check the login here.
                     } else {
                         System.out.println("Login");
@@ -126,6 +126,7 @@ public class Login_in extends HttpServlet {
                 //Getting friendlist and the username list for the search bar of firent
                 List<Friend> friendList = new FriendDAO().selectionListOfFriends(innerclass.username);
                 List<String> userList = new FriendDAO().GetAllPeopleUsername();
+                new FriendDAO().pullAllFriendProfile(friendList);
                 session.setAttribute("firendlist", friendList);
                 session.setAttribute("userlist", userList);
                 System.out.println("logged-in");
@@ -142,70 +143,43 @@ public class Login_in extends HttpServlet {
             }
         } catch (Exception e) {
             cookieTracker(req, resp);
-    public boolean loginLogic(HttpServletRequest req, HttpServletResponse resp, HttpSession session, LoginPassing loginPassing) throws ServletException, IOException {
-
-
-        if (loginPassing.selectionUsersNames(username, password)) {
-            //Login in logic passed through sql selection query.
-            session.setAttribute("username", username);
-            session.setAttribute("password", password);
-            session.setAttribute("articleList", "self");
-            session.setAttribute("log", true); //TODO refactoring for login status.
-            //Getting friendlist and the username list for the search bar of firent
-            List<Friend> friendList = new FriendDAO().selectionListOfFriends(username);
-            List<String> userList = new FriendDAO().GetAllPeopleUsername();
-            new FriendDAO().pullAllFriendProfile(friendList);
-            System.out.println("Friends profile pic added");
-
-            session.setAttribute("firendlist", friendList);
-            session.setAttribute("userlist", userList);
-            System.out.println("logged-in");
-            System.out.println(session.getAttribute("username"));
-            req.getRequestDispatcher("/ProfilePage").forward(req, resp); //TODO to take out.
-            return true;
-        } else {
-            //Login in logic fail.
-            session.setAttribute("log", false); //TODO refactoring for login status.
-            System.out.println("logged-in rejected");
-            req.setAttribute("loginFail", true); //attribute to indicate to login_page jsp to shake if rejected onload
-            req.getRequestDispatcher("/login_page.jsp").forward(req, resp);
-            return false;
         }
         return false;
     }
 
 
-    //doGet is when a GET request sent from the login page where the user wants to goto the register page to register a new account.
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        try {
-            HttpSession session = req.getSession();
-            String registration = req.getParameter("Registration");
-            session.setAttribute("Registration", false);
-            if (registration != null) { //TODO refactor to switch statement.
-                //checking if user is login already bound back to profilepage when registration is clicked.
-                if (session.getAttribute("log") == null) {
-                    req.getRequestDispatcher("/WEB-INF/webthings/registration_page.jsp").forward(req, resp);
-                    return;
-                }
-                //If the user is logined in already the user will be transfer back to his own profilepage.
-                if ((boolean) session.getAttribute("log")) {
-                    req.getRequestDispatcher("/ProfilePage").forward(req, resp);
-                    return;
-                } else {
-                    //If user is not login then it will bring them to the registration page.
-                    if (registration.equals("Register")) {
-                        session.setAttribute("Upload", "ArticlesUpload");
+
+        //doGet is when a GET request sent from the login page where the user wants to goto the register page to register a new account.
+        @Override
+        protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            try {
+                HttpSession session = req.getSession();
+                String registration = req.getParameter("Registration");
+                session.setAttribute("Registration", false);
+                if (registration != null) { //TODO refactor to switch statement.
+                    //checking if user is login already bound back to profilepage when registration is clicked.
+                    if (session.getAttribute("log") == null) {
                         req.getRequestDispatcher("/WEB-INF/webthings/registration_page.jsp").forward(req, resp);
                         return;
                     }
+                    //If the user is logined in already the user will be transfer back to his own profilepage.
+                    if ((boolean) session.getAttribute("log")) {
+                        req.getRequestDispatcher("/ProfilePage").forward(req, resp);
+                        return;
+                    } else {
+                        //If user is not login then it will bring them to the registration page.
+                        if (registration.equals("Register")) {
+                            session.setAttribute("Upload", "ArticlesUpload");
+                            req.getRequestDispatcher("/WEB-INF/webthings/registration_page.jsp").forward(req, resp);
+                            return;
+                        }
+                    }
+                } else {
+                    //Anything else bounce!
+                    cookieTracker(req, resp);
                 }
-            } else {
-                //Anything else bounce!
+            } catch (Exception e) {
                 cookieTracker(req, resp);
             }
-        } catch (Exception e) {
-            cookieTracker(req, resp);
         }
     }
-}
--- web/component/NavBar-AfterLogin(Template).jsp
+++ web/component/NavBar-AfterLogin(Template).jsp
@@ -80,7 +80,6 @@
                         <c:when test="${profileInfo.profilepic != null}">
                             <c:choose>
                                 <%--If this is a default profile image get the image from default photo directory--%>
-                                <c:when test='${profileInfo.profilepic.startsWith("default")}'>
                                 <c:when test='${profileInfo.profilepic.startsWith("defaultslashn")}'>
                                     <img src="defaultImg/${profileInfo.profilepic}"
                                          height="20">
