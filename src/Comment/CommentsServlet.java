d5258e3248c91d9a6bdd6b9528c8db9eecd6208c
ljam763 <ljam763@aucklanduni.ac.nz>
2017-06-15 17:08:00 +1200

    "fixed article serv"

--- src/Comment/CommentsServlet.java
+++ src/Comment/CommentsServlet.java
@@ -130,6 +130,7 @@ public class CommentsServlet extends HttpServlet {
                     innerclass.setComments(innerclass.commentsDAO.selectionComment(commentId));
 //                comments = commentsDAO.selectionComment(commentId);
                 }
+                System.out.println(innerclass.comments);
                 if (innerclass.comments != null) {
                     JSONObject jsonObject = getJsonObject(innerclass.comments);
                     System.out.println(jsonObject);
@@ -189,6 +190,7 @@ public class CommentsServlet extends HttpServlet {
         System.out.println(session.getAttribute("articleID"));
         innerclass.setArticleID((int) session.getAttribute("articleID"));
 //        articleID = (int) session.getAttribute("articleID");
+
     }
 
     @Override
